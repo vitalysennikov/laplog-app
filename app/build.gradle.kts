036852d3a4d595c0c9419aca5f6ffeb26a67ebf3
Vitaly Sennikov <vitalysennikov@users.noreply.github.com>
2026-04-26 13:49:20 +0300

    Версия 0.15.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.laplog.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 61
-        versionName = "0.15.4"
+        versionCode = 62
+        versionName = "0.15.5"
 
         vectorDrawables {
             useSupportLibrary = true
